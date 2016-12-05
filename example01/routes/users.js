c68d9b5fb9ce5322b822f742dfab117f6c212b95
Kim-Jin-ho <jinho528@naver.com>
2016-12-05 10:22:44 +0900

    commit messages

--- example01/routes/users.js
+++ example01/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('정상적으로 작동하고 있습니다~');
 });
 
 module.exports = router;
